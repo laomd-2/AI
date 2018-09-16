88dbc4e07c8df96f99f0f6cd8919b0e5bad30b7d
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-09-16 22:35:07 +0800

    调整代码结构

--- Lab1-Python/code/KNN/KNN.py
+++ Lab1-Python/code/KNN/KNN.py
@@ -1,7 +1,21 @@
-from knn_collections import PriorityQueue
-from collections import Counter
+from heapq import *
 
 __all__ = ["KNN"]
+
+
+class PriorityQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def __len__(self):
+        return len(self.queue)
+
+    def put(self, item):
+        heappush(self.queue, item)
+
+    def get(self):
+        return heappop(self.queue)
 
 
 class KNN:
