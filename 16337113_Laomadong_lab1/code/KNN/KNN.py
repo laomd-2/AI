e5d32eb99e7f416c01b38597387795da7d1ef86e
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-09-21 09:06:20 +0800

    实验报告

--- 16337113_Laomadong_lab1/code/KNN/KNN.py
+++ 16337113_Laomadong_lab1/code/KNN/KNN.py
@@ -31,17 +31,14 @@ class KNN:
         self._trained_datas.append((trained_x, trained_y))
 
     def predict(self, test_x):
-        # (余弦距离，label)二元组的最小堆
         k_neighbors = PriorityQueue()
         for other_x, y in self._trained_datas:
             dis = self._distance_policy(test_x, other_x)
             if math.fabs(dis - self._distance_policy(other_x, other_x)) < 0.000001:
                 return y
             k_neighbors.put((dis, y))
-            # 至多存储k个邻居
             if len(k_neighbors) > self._k:
                 k_neighbors.get()
-        # 从k个最近邻居中决定测试样本的label的策略，用户代码中自定义
-        # 好处在于可以针对不同类型问题（如分类和回归），不同加权方法得到label
         label = self._label_policy(k_neighbors)
+        # self.add(test_x, label)
         return label
