e34afd1c5bcba23570fd169f532655dc4eabe39f
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-10-13 00:43:26 +0800

    去掉for循环

--- logisticRegression/logistic_model/cost.py
+++ logisticRegression/logistic_model/cost.py
@@ -1,19 +1,37 @@
 import numpy as np
 
 
+def acc(y, y_pred):
+    return y[y == y_pred].size / y.size
+
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 def cross_entropy(W, X, y):
+    W = np.ravel(W)
+    y = np.ravel(y)
     h_x = sigmoid(X.dot(W.T))
     first = np.multiply(-y, np.log(h_x))
     second = np.multiply(1-y, np.log(1 - h_x))
     return np.sum(first - second) / len(X)
 
+def regularized_cross_entropy(W, X, y, learning_rate):
+    ce = cross_entropy(W, X, y)
+    W = np.ravel(W)
+    y = np.ravel(y)
+    reg = (learning_rate / 2 * len(X)) * np.sum(np.power(W[1:W.shape[0]], 2))
+    return ce + reg
+
 def gradient_ce(W, X, y):
+    W = np.ravel(W)
+    y = np.ravel(y)
     error = sigmoid(X.dot(W.T)) - y
-    grad = np.zeros_like(W)
-    for i in range(W.shape[0]):
-        term = np.multiply(error, X[:, i])
-        grad[i] += np.sum(term)
+    grad = np.sum(np.multiply(error, X.T).T, axis=0)
+    return grad / len(X)
+
+def regularized_gradient_ce(W, X, y, learning_rate):
+    W = np.ravel(W)
+    y = np.ravel(y)
+    error = sigmoid(X.dot(W.T)) - y
+    grad = np.sum(np.multiply(error, X.T).T, axis=0) + learning_rate * W
     return grad / len(X)
--- logisticRegression/logistic_model/logisticRegression.py
+++ logisticRegression/logistic_model/logisticRegression.py
@@ -1,25 +1,42 @@
 import numpy as np
 import scipy.optimize as opt
-from .cost import sigmoid, cross_entropy, gradient_ce
+from .cost import *
 
 
 class LogisticRegression:
     
     def __init__(self):
-        self.__W = None
+        self.W = None
     
     @staticmethod
     def argmented(M):
         return np.column_stack((np.ones(M.shape[0]), M))
-        
+
+    def predict(self, X):
+        X = self.argmented(X)
+        prob = sigmoid(X.dot(self.W.T))
+        return np.asarray([1 if p > 0.5 else 0 for p in prob])
+
+
+class SimpleLogisticRegression(LogisticRegression):
+    
     def fit(self, X, y):
         X = self.argmented(X)
         W = np.zeros(X.shape[1])
         result = opt.fmin_tnc(func=cross_entropy, x0=W, 
                               fprime=gradient_ce, args=(X, y))
-        self.__W = result[0]
+        self.W = result[0]
+        
 
-    def predict(self, X):
+class RegularizedLogisticRegression(LogisticRegression):
+        
+    def __init__(self, learning_rate):
+        self.__learning_rate = learning_rate
+    
+    def fit(self, X, y):
         X = self.argmented(X)
-        prob = sigmoid(X.dot(self.__W.T))
-        return np.asarray([1 if p > 0.5 else 0 for p in prob])
+        W = np.zeros(X.shape[1])
+        result = opt.fmin_tnc(func=regularized_cross_entropy, x0=W, 
+                              fprime=regularized_gradient_ce, args=(X, y, self.__learning_rate))
+        self.W = result[0]
+    
