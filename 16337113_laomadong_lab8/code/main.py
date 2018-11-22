065a761bf536cece59e02f4e45717a3ebf0d2a1f
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-11-22 11:36:17 +0800

    αβ剪枝

--- 16337113_laomadong_lab8/code/main.py
+++ 16337113_laomadong_lab8/code/main.py
@@ -1,34 +1,24 @@
-from point import Point
 from board import Board
-from minimax import MiniMax
+from gambling.minimax import MiniMax
+from gambling.alpha_beta import AlphaBeta
 import numpy as np
 
 
 if __name__ == '__main__':
-    first, second = 'O', 'X'
-    board = Board(np.array(np.loadtxt("../data/board.txt", dtype=str)), first, second, '*')
-    computer = MiniMax(board, second, first, '*', False)
+    board = Board(np.array(np.loadtxt("../data/board.txt", dtype=str)))
+    computers = [MiniMax(board, True), AlphaBeta(board, False)]
     turn = 0
     print(board)
     while not board.game_over():
-        if turn == 0:
-            move = tuple(map(int, input("your turn:").split(' ')))
-            while not board.apply_move(Point(move), first):
-                move = tuple(map(int, input("invalid move, input again:").strip().split(' ')))
-        else:
-            print("computer thinking...", end=' ')
-            move = computer.decide()
-            if move is not None:
-                print(move)
-                board.apply_move(move, second)
-            else:
-                print("cannot move")
+        computer = computers[turn]
+        print(computer.__class__.__name__, "thinking...", end=' ')
+        print(computer.decide_and_drop())
         turn = not turn
         print(board)
     score = board.score()
     if score > 0:
-        print(first, "win!")
+        print(board.FIRST, "win!")
     elif score < 0:
-        print(second, "win!")
+        print(board.SECOND, "win!")
     else:
         print("平局")
