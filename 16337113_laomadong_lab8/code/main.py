fbc5502c8d2594e8c38bb09dd491af8ef46b9c8c
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-11-28 10:10:27 +0800

    人机对战

--- 16337113_laomadong_lab8/code/main.py
+++ 16337113_laomadong_lab8/code/main.py
@@ -1,4 +1,5 @@
 from board import Board
+from point import Point
 from gambling.negamaxalphabeta import NegamaxAlphaBeta
 from gambling.minimaxalphabeta import MinimaxAlphaBeta
 import numpy as np
@@ -6,15 +7,20 @@ from heuristic import difference, sigmoid
 
 
 if __name__ == '__main__':
-    board = Board(np.array(np.loadtxt("../data/board4.txt", dtype=str)))
-    computers = [NegamaxAlphaBeta(board, sigmoid, True), MinimaxAlphaBeta(board, sigmoid, False)]
+    board = Board(np.array(np.loadtxt("../data/board8.txt", dtype=str)))
+    # computers = [NegamaxAlphaBeta(board, sigmoid, True), MinimaxAlphaBeta(board, sigmoid, False)]
+    computer = NegamaxAlphaBeta(board, difference, True)
     turn = 0
     print(board)
     print()
     while not board.game_over():
-        computer = computers[turn]
-        print("computer", turn + 1, "thinking...", end=' ')
-        print(computer.decide_and_drop(8))
+        # computer = computers[turn]
+        if turn == 0:
+            print("computer", turn + 1, "thinking...", end=' ')
+            print(computer.decide_and_drop(8))
+        else:
+            move = tuple(map(int, input("your turn:").strip().split()))
+            board.apply_move(Point(move), Board.SECOND)
         turn = not turn
         print(board)
         print("score:", difference(board))
